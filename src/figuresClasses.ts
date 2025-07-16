d3df3154ab2c73b2d5e4a592ca0b7f4cfa207956
user <graban2253@gmail.com>
2025-07-17 01:24:58 +0300

    Solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -49,7 +49,7 @@ export class Circle implements Figure {
     public radius: number,
   ) {
     if (radius <= 0) {
-      throw new Error('Radius can`t be less than 0');
+      throw new Error('Radius must be greater than 0');
     }
   }
 
